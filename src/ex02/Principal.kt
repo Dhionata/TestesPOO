3253f15584be30bda02901dc36fad2ccf85ab940
Dhionata <xiyunagi@gmail.com>
2020-03-14 18:44:03 -0300

    1.5.0.1 "esqueci de tirar os comentários.. kk"

--- src/ex02/Principal.kt
+++ src/ex02/Principal.kt
@@ -5,9 +5,9 @@ import java.util.Scanner
 val entrada: Scanner = Scanner(System.`in`)
 fun main() {
 
-    /*val televisoes = mutableListOf<Televisor>()
+    val televisoes = mutableListOf<Televisor>()
     println("Quer criar quantos televisores? ")
-    val x = entrada.nextInt()*/
+    val x = entrada.nextInt()
 
     val televisor1 = Televisor(false, 10)
     televisor1.ligar()
@@ -19,7 +19,7 @@ fun main() {
     televisor1.adicionarCanal(canal3)
 
     televisor1.imprimir()
-    //televisor1.criarCanal()
+    televisor1.criarCanal()
     televisor1.canalAcima()
     televisor1.imprimir()
     televisor1.removerCanal(canal2)
@@ -28,136 +28,135 @@ fun main() {
     televisor1.volumeAcima()
     televisor1.volumeAbaixo()
     televisor1.desligar()
-}
 
-/*repeat(x) {
-    val televisor = Televisor()
-    do {
-        println("\nDeseja ligar ou desligar?")
-        val y = entrada.next()
-        when (y) {
-            "desligar" -> {
-                televisor.desligar()
-            }
-            "ligar" -> {
-                televisor.ligar()
-                println("Por favor adicione um canal")
-                televisor.criarCanal()
-                do {
-                    println("\nDeseja criar e adicionar mais canais?\nSim - Não")
-                    var simNao = entrada.next()
-                    when (simNao) {
-                        "Sim" -> {
-                            televisor.criarCanal()
-                        }
-                        "Não" -> {
-                            do {
-                                println("\nInforme o volume da televisão: ")
-                                val volume = entrada.nextByte()
-                                if (volume in 0..80) {
-                                    televisor.volume = volume
-                                } else {
-                                    println("\nDigite um valor de 0 à 80")
-                                    televisor.volume = -1
-                                }
-                            } while (televisor.volume.toInt() == -1)
-                            do {
-                                println("\nDeseja passar ou voltar o canal?\nSim - Não")
-                                simNao = entrada.next()
-                                when (simNao) {
-                                    "Sim" -> {
-                                        do {
-                                            do {
-                                                println("\nDeseja aumentar ou diminuir? ")
-                                                var aumentarDiminuir = entrada.next()
-                                                when (aumentarDiminuir) {
-                                                    "aumentar" -> {
-                                                        televisor.canalAcima()
-                                                    }
-                                                    "diminuir" -> {
-                                                        televisor.canalAbaixo()
-                                                    }
-                                                    else -> {
-                                                        println("tente novamente...")
-                                                        aumentarDiminuir = null
-                                                    }
-                                                }
-                                            } while (aumentarDiminuir.isEmpty())
-                                            println("\nDeseja aumentar ou diminuir denovo?\nSim - Não")
-                                            val mais = entrada.next()
-                                        } while (mais != "Não")
+    repeat(x) {
+        val televisor = Televisor()
+        do {
+            println("\nDeseja ligar ou desligar?")
+            val y = entrada.next()
+            when (y) {
+                "desligar" -> {
+                    televisor.desligar()
+                }
+                "ligar" -> {
+                    televisor.ligar()
+                    println("Por favor adicione um canal")
+                    televisor.criarCanal()
+                    do {
+                        println("\nDeseja criar e adicionar mais canais?\nSim - Não")
+                        var simNao = entrada.next()
+                        when (simNao) {
+                            "Sim" -> {
+                                televisor.criarCanal()
+                            }
+                            "Não" -> {
+                                do {
+                                    println("\nInforme o volume da televisão: ")
+                                    val volume = entrada.nextByte()
+                                    if (volume in 0..80) {
+                                        televisor.volume = volume
+                                    } else {
+                                        println("\nDigite um valor de 0 à 80")
+                                        televisor.volume = -1
                                     }
-                                    "Não" -> {
-                                        do {
-                                            println("\nDeseja aumentar ou diminuir o volume?\nSim - Não")
-                                            simNao = entrada.next()
-                                            when (simNao) {
-                                                "Sim" -> {
-                                                    do {
-                                                        println("\nDeseja aumentar ou diminuir?")
-                                                        val aumentarDiminuir = entrada.next()
-                                                        val vezes = entrada.nextInt()
-                                                        repeat(vezes) {
-                                                            when (aumentarDiminuir) {
-                                                                "aumentar" -> {
-                                                                    televisor.volumeAcima()
+                                } while (televisor.volume.toInt() == -1)
+                                do {
+                                    println("\nDeseja passar ou voltar o canal?\nSim - Não")
+                                    simNao = entrada.next()
+                                    when (simNao) {
+                                        "Sim" -> {
+                                            do {
+                                                do {
+                                                    println("\nDeseja aumentar ou diminuir? ")
+                                                    var aumentarDiminuir = entrada.next()
+                                                    when (aumentarDiminuir) {
+                                                        "aumentar" -> {
+                                                            televisor.canalAcima()
+                                                        }
+                                                        "diminuir" -> {
+                                                            televisor.canalAbaixo()
+                                                        }
+                                                        else -> {
+                                                            println("tente novamente...")
+                                                            aumentarDiminuir = null
+                                                        }
+                                                    }
+                                                } while (aumentarDiminuir.isEmpty())
+                                                println("\nDeseja aumentar ou diminuir denovo?\nSim - Não")
+                                                val mais = entrada.next()
+                                            } while (mais != "Não")
+                                        }
+                                        "Não" -> {
+                                            do {
+                                                println("\nDeseja aumentar ou diminuir o volume?\nSim - Não")
+                                                simNao = entrada.next()
+                                                when (simNao) {
+                                                    "Sim" -> {
+                                                        do {
+                                                            println("\nDeseja aumentar ou diminuir?")
+                                                            val aumentarDiminuir = entrada.next()
+                                                            val vezes = entrada.nextInt()
+                                                            repeat(vezes) {
+                                                                when (aumentarDiminuir) {
+                                                                    "aumentar" -> {
+                                                                        televisor.volumeAcima()
+                                                                    }
+                                                                    "diminuir" -> {
+                                                                        televisor.volumeAbaixo()
+                                                                    }
+                                                                    else -> {
+                                                                        println("\nTenho que aprender outros idiomas...")
+                                                                    }
                                                                 }
-                                                                "diminuir" -> {
-                                                                    televisor.volumeAbaixo()
+                                                            }
+                                                        } while (aumentarDiminuir != "Sim" && aumentarDiminuir != "Não")
+                                                    }
+                                                    "Não" -> {
+                                                        do {
+                                                            println("\nDeseja remover algum canal?\nSim - Não")
+                                                            simNao = entrada.next()
+                                                            when (simNao) {
+                                                                "Sim" -> {
+                                                                    val canalAux = Canal()
+                                                                    println("\nInforme o nome do canal: ")
+                                                                    canalAux.nome = entrada.next()
+                                                                    println("\nInforme o número do canal: ")
+                                                                    canalAux.numero = entrada.nextByte()
+                                                                    televisor.removerCanal(canalAux)
+                                                                }
+                                                                "Não" -> {
+                                                                    println("\nVocê configurou tudo meu amigo! valeu...")
+                                                                    televisor.imprimir()
                                                                 }
                                                                 else -> {
-                                                                    println("\nTenho que aprender outros idiomas...")
+                                                                    println("\nEscolha corretamente dessa vez em...")
                                                                 }
                                                             }
-                                                        }
-                                                    } while (aumentarDiminuir != "Sim" && aumentarDiminuir != "Não")
+                                                        } while (simNao != "Sim" && simNao != "Não")
+                                                    }
+                                                    else -> {
+                                                        println("\nNovos idiomas não serão adicionados mano...")
+                                                    }
                                                 }
-                                                "Não" -> {
-                                                    do {
-                                                        println("\nDeseja remover algum canal?\nSim - Não")
-                                                        simNao = entrada.next()
-                                                        when (simNao) {
-                                                            "Sim" -> {
-                                                                val canalAux = Canal()
-                                                                println("\nInforme o nome do canal: ")
-                                                                canalAux.nome = entrada.next()
-                                                                println("\nInforme o número do canal: ")
-                                                                canalAux.numero = entrada.nextByte()
-                                                                televisor.removerCanal(canalAux)
-                                                            }
-                                                            "Não" -> {
-                                                                println("\nVocê configurou tudo meu amigo! valeu...")
-                                                                televisor.imprimir()
-                                                            }
-                                                            else -> {
-                                                                println("\nEscolha corretamente dessa vez em...")
-                                                            }
-                                                        }
-                                                    } while (simNao != "Sim" && simNao != "Não")
-                                                }
-                                                else -> {
-                                                    println("\nNovos idiomas não serão adicionados mano...")
-                                                }
-                                            }
-                                        } while (simNao != "Sim" && simNao != "Não")
+                                            } while (simNao != "Sim" && simNao != "Não")
+                                        }
+                                        else -> {
+                                            println("\nNão entendo outras palavras...")
+                                        }
                                     }
-                                    else -> {
-                                        println("\nNão entendo outras palavras...")
-                                    }
-                                }
-                            } while (simNao != "Sim" && simNao != "Não")
+                                } while (simNao != "Sim" && simNao != "Não")
+                            }
+                            else -> {
+                                println("\nInforme corretamente sua resposta!")
+                            }
                         }
-                        else -> {
-                            println("\nInforme corretamente sua resposta!")
-                        }
-                    }
-                } while (simNao != "Não")
+                    } while (simNao != "Não")
+                }
+                else -> {
+                    println("\nVocê digitou errado...")
+                }
             }
-            else -> {
-                println("\nVocê digitou errado...")
-            }
-        }
-    } while (y != "ligada" && y != "desligada")
-    televisoes.add(televisor)
+        } while (y != "ligada" && y != "desligada")
+        televisoes.add(televisor)
+    }
 }
-}*/
